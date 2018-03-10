2240c4d32e93d71f01f4f307f8aab1d96c99797e
bhayanisohil <sohil.bhayani@yahoo.com>
2018-03-10 15:05:51 -0500

    Add files via upload

---
+++ AutomateDBExports/src/Cosntants.java
@@ -0,0 +1,8 @@
+
+public class Cosntants {
+	
+	public static final String DB_NAME="ntfjvv";
+	public static final String DB_USER_NAME="root";
+	public static final String DB_PASSWORD="admin@DJ2018";
+
+}
---
+++ AutomateDBExports/src/DatabaseQueries.java
@@ -0,0 +1,52 @@
+import java.sql.*;
+
+public class DatabaseQueries {
+
+	public static int getLatestIdinForm(String formName) throws ClassNotFoundException, SQLException {
+		int id = 0;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/" + Cosntants.DB_NAME,
+					Cosntants.DB_USER_NAME, Cosntants.DB_PASSWORD);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM " + formName + " ORDER BY id DESC LIMIT 1");
+			while (rs.next())
+				System.out.println("latest id in table " + formName + " is " + rs.getInt(1));
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return id;
+	}
+
+	public static Connection getDBConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/" + Cosntants.DB_NAME,
+				Cosntants.DB_USER_NAME, Cosntants.DB_PASSWORD);
+		return con;
+	}
+
+	public static int getLatestIdFromTable(Connection con, String formName) throws SQLException {
+		int id = 0;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM " + formName + " ORDER BY id DESC LIMIT 1");
+			while (rs.next()) {
+				System.out.println("latest id in table " + formName + " is " + rs.getInt(1));
+				id = rs.getInt(1);
+			}
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
+
+		return id;
+	}
+
+}
